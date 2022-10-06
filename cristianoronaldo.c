8ac30074c17ca6edc3b6cace3d71a7863dd5d163
César Martín <cesar@CesarSurface.localdomain>
2022-10-06 18:40:20 +0200

    Solved bug in cr7 otra vez

--- cristianoronaldo.c
+++ cristianoronaldo.c
@@ -5,7 +5,7 @@ int cristianoronaldo (){
 	printf("No le pongas el punto extra a César");
 
     	for(int i = 0; i<10; i++){
-		printf("\Caralho, a programasao no lenguaje C e muito complicada\nSIUUU\n");
+		printf("\nCaralho, a programasao no lenguaje C e muito complicada\nSIUUU\n");
 	
 	}
 
