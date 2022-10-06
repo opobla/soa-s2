345e5d0d73144c9ef50bee09ecd3538fcb96da11
sergilm <s.lorenzo@edu.uah.es>
2022-10-06 18:29:03 +0200

    Añadida nueva función

--- sergilm.c
+++ sergilm.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 
+int elBicho(){
+        for (int i = 0; i<7; i++){
+                printf("Hehehe...siuuuuuuuu")
+        }
+        return (0);
+}
+
+
 int sergio(){
-	printf("Hola soy Sergio ;)");
+    printf("Hola soy Sergio ;)");
+    elBicho();
     return (0);
-}	
+}
+
